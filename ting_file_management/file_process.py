6948a52a51cf643e8953c667fe22c77b66974375
Mateus Bertazzo <mateusbataglin52@gmail.com>
2023-09-28 15:02:58 -0300

    resolve req 3

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,22 @@
+from ting_file_management.file_management import txt_importer
+import sys
+
+
 def process(path_file, instance):
     """Aqui irá sua implementação"""
+    for item in list(instance.queue):
+        if item["nome_do_arquivo"] == path_file:
+            return
+    info_txt = txt_importer(path_file)
+
+    data = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(info_txt),
+        "linhas_do_arquivo": info_txt
+    }
+
+    instance.enqueue(data)
+    sys.stdout.write(str(data))
 
 
 def remove(instance):
