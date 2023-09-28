92763b433a6df46277694346e0cdde870e4cd583
Mateus Bertazzo <mateusbataglin52@gmail.com>
2023-09-28 17:06:36 -0300

    finalmente resolve req 6

--- tests/priority_queue/test_priority_queue.py
+++ tests/priority_queue/test_priority_queue.py
@@ -1,5 +1,40 @@
-# from ting_file_management.priority_queue import PriorityQueue
+from ting_file_management.priority_queue import PriorityQueue
+import pytest
 
 
 def test_basic_priority_queueing():
     """Aqui irá sua implementação"""
+    mock = [
+        {"qtd_linhas": 5},
+        {"qtd_linhas": 3},
+        {"qtd_linhas": 8},
+        {"qtd_linhas": 2},
+    ]
+
+    result_priority_queue = PriorityQueue()
+
+    result_priority_queue.enqueue(mock[0])
+    result_priority_queue.enqueue(mock[1])
+    result_priority_queue.enqueue(mock[2])
+    result_priority_queue.enqueue(mock[3])
+
+    assert len(result_priority_queue) == 4
+    assert result_priority_queue.search(0) == mock[1]
+    assert result_priority_queue.search(1) == mock[3]
+    assert result_priority_queue.search(2) == mock[0]
+    assert result_priority_queue.search(3) == mock[2]
+
+    result_priority_queue.dequeue()
+    assert len(result_priority_queue) == 3
+    assert result_priority_queue.search(0) == mock[3]
+
+    result_priority_queue.dequeue()
+    assert len(result_priority_queue) == 2
+    assert result_priority_queue.search(0) == mock[0]
+
+    result_priority_queue.dequeue()
+    assert len(result_priority_queue) == 1
+    assert result_priority_queue.search(0) == mock[2]
+
+    with pytest.raises(IndexError, match="Índice Inválido ou Inexistente"):
+        result_priority_queue.search(26)
