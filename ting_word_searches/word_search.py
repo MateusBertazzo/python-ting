8a1df9da8c581c778e3723e5d4efb06d13ac1e0f
Mateus Bertazzo <mateusbataglin52@gmail.com>
2023-09-28 16:09:29 -0300

    resolve req 7

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,22 @@
 def exists_word(word, instance):
     """Aqui irá sua implementação"""
+    result = []
+
+    def is_word_in_line(line):
+        return word.lower() in line.lower()
+
+    for new in instance.queue:
+        info_txt = {
+            "palavra": word,
+            "arquivo": new["nome_do_arquivo"],
+            "ocorrencias": [
+                {"linha": line_index + 1} for line_index, line in enumerate(
+                    new["linhas_do_arquivo"]) if is_word_in_line(line)]
+        }
+        if info_txt["ocorrencias"]:
+            result.append(info_txt)
+
+    return result
 
 
 def search_by_word(word, instance):
