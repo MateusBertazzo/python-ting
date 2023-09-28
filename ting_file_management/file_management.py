b690d45eedf6e7f3a4535efdbd4f16a480489429
Mateus Bertazzo <mateusbataglin52@gmail.com>
2023-09-28 14:36:09 -0300

    resolve req 2

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,12 @@
+import sys
+
+
 def txt_importer(path_file):
     """Aqui irá sua implementação"""
+    try:
+        if not path_file.endswith(".txt"):
+            sys.stderr.write("Formato inválido")
+        with open(path_file) as file:
+            return file.read().split("\n")
+    except FileNotFoundError:
+        sys.stderr.write(f"Arquivo {path_file} não encontrado\n")
